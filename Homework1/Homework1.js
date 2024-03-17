6bb8cd05a5d33944fabc93e0f242b765b953fd9d
mariiabolonkina <122316377+mariiabolonkina@users.noreply.github.com>
2024-03-17 18:52:10 +0200

    Add files via upload

---
+++ Homework1/Homework1.js
@@ -0,0 +1,63 @@
+// //Task2
+// alert("Bolonkina");
+
+// //Task3
+// let serverName = "DXEServerMachine";
+// let workstationName = "DXEWorkstationMachine";
+// console.log(serverName,workstationName);
+// serverName = workstationName;
+// console.log(serverName,workstationName);
+
+//Task4
+// let office = {
+//     officeName: "OfficeLab",
+//     foundationYear: 2006,
+//     works: true,
+//     guests: undefined,
+//     numberOfOwners: null,
+//};
+//console.log(office);
+
+//Task5
+//let isAdult = confirm("Are you over 18 years old?");
+//alert (isAdult);
+
+//Task6
+// let firstName = "Mariia";
+// console.log(typeof firstName);
+// let lastName = "Bolonkina";
+// console.log(typeof lastName);
+// let group = "TA-Q1=2024.Training";
+// console.log(typeof group);
+// const birthday = 1999;
+// console.log(typeof birthday);
+// let marriedStatus = false;
+// console.log(typeof marriedStatus);
+
+// console.log(birthday,marriedStatus,firstName,lastName,group);
+
+// let data1 = null;
+// console.log(typeof data1);
+// let data2;
+// console.log(typeof data2);
+
+//Task7
+// let login = prompt("What is your Login?");
+// let email = prompt("What is your E-mail?");
+// let password = prompt("What is your Password?");
+// console.log("Dear", login,", your email is", email,", your password is", password);
+
+//Task8
+// let x = "60";
+// let y = "60";
+// let z = x * y;
+// console.log("1 hour =", z,"seconds");
+// let a = "60";
+// let b = "60";
+// let h = "24";
+// let s = a * b * h;
+// console.log("24 hours =", s,"seconds");
+// let m = prompt("How many days are in the current month?");
+// let d = "86400";
+// let f = m * d;
+// console.log("current month =", f,"seconds");
