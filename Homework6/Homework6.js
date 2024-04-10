2d19e309706671be9f9379236acce8cbb73229dd
mariiabolonkina <122316377+mariiabolonkina@users.noreply.github.com>
2024-04-10 21:02:18 +0300

    Homework6.js

---
+++ Homework6/Homework6.js
@@ -0,0 +1,73 @@
+//task1
+//document.getElementById("test").innerHTML = "Last";
+//test.innerHTML = '<div id="test">Last</div>';
+
+//task2
+
+// let imgElement = document.querySelector('.image');
+// imgElement.src = 'https://en.m.wikipedia.org/wiki/File:Adorable-animal-cat-20787.jpg';
+// alert(imgElement.outerHTML);
+
+//task3
+
+// var tests = document.querySelectorAll('#text p');
+// tests.forEach(function(test, index) {
+//     console.log("Selector text" + index + ": " + test.textContent);
+// });
+
+//task4
+to be done
+
+//taks5
+// let header = document.querySelector('h1');
+// let paragraphs = document.querySelectorAll('#myDiv p');
+// let listItems = document.querySelectorAll('#myDiv p');
+// let span = document.querySelectorAll('span');
+
+// header.style.fontWeight = 'bold';
+// header.style.color = 'Black';
+// header.style.backgroundcolor = '#008000';
+// header.style.display = 'inline-block';
+
+// paragraphs[0].style.fontWeight = 'bold';
+// paragraphs[0].style.color = 'black';
+
+// paragraphs[1].style.color = 'red';
+
+// paragraphs[2].style.textDecoration = 'underline';
+
+// paragraphs[3].style.fontstyle = 'italic';
+
+// listItems.forEach(function(item){
+//     item.style.display = 'inline-block';
+// });
+
+// span.style.display = 'none'; //do not visiable
+
+//task6
+// var message1 = prompt("Введіть перше повідомлення");
+// var message2 = prompt("Введіть друге повідомлення");
+
+// document.getElementById("input1").value = message1;
+// document.getElementById("input2").value = message2;
+
+// var temp = document.getElementById("input").value;
+// document.getElementById("input1").value = document.getElementById("input2").value;
+// document.getElementById("input2").value = temp;
+
+//task7
+// var body = document.createElement('body');
+// var main = document.createElement('main');
+// var div = document.createElement('div');
+// var paragraph = document.createElement('p');
+
+// main.className = 'mainClass check item';
+
+// div.id = 'myDiv';
+
+// paragraph.textContent = 'First paragraph';
+// div.appendChild(paragraph);
+// main.appendChild(div);
+// body.appendChild(main);
+
+// document.documentElement.appendChild(body);
