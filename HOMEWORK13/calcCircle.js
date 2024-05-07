f0ea52be1162a64f8e7b649bc5f73928a6b6a403
mariiabolonkina <122316377+mariiabolonkina@users.noreply.github.com>
2024-05-07 13:02:59 +0300

    calcCircle.js

--- HOMEWORK13/calcCircle.js
+++ HOMEWORK13/calcCircle.js
@@ -1,1 +1,14 @@
+function getCircleLength(radius) {
 
+    return 2 * Math.PI * radius;
+    
+    }
+    
+function getCircleArea(radius) {
+    
+    return Math.PI * radius ** 2;
+    
+    }
+
+module.exports = {getCircleLength};
+module.exports = {getCircleArea};
