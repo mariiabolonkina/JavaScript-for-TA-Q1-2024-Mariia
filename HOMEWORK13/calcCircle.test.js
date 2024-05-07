e7cb0609b2a6924ba2476dfb7ed321c283d8c317
mariiabolonkina <122316377+mariiabolonkina@users.noreply.github.com>
2024-05-07 13:03:36 +0300

    calcCircle.test.js

---
+++ HOMEWORK13/calcCircle.test.js
@@ -0,0 +1,15 @@
+const {getCircleLength} = require('./calcCircle');
+const {getCircleArea} = require('./calcCircle');
+
+xdescribe("GetCircleLength function", () => {
+    test('When circle radius is 22 the circle length is equal to 138.2', () => {
+        expect(getCircleLength(22)).toBeCloseTo(138.2);
+    });
+});
+
+describe("GetCircleArea function", () => {
+    test('When circle radius is 9 the circle area is near to 254.47', () => {
+        expect(getCircleArea(9)).toBeCloseTo(254.47);
+    });
+});
+
