13049afeaef5aa57b91005a7fe7c35cabc3598fe
mariiabolonkina <122316377+mariiabolonkina@users.noreply.github.com>
2024-05-07 12:53:20 +0300

    userList.test.js

--- HOMEWORK13/userList.test.js
+++ HOMEWORK13/userList.test.js
@@ -1,1 +1,24 @@
+import userList from ('./userList.js');
 
+const userList = ['Nick', 'Kate', 'quest123', 'admin', 'new_user_2'];
+
+describe("Testing array", () => {
+    test('UserList has "admin" element on it', () => {
+        expect(userList).toContain('admin');
+    });
+    test('The first element is "Nick"', () => {
+        expect(userList[0]).toBe('Nick');
+    });
+    test('The last element is “new_user_2”', () => {
+        expect(userList.slice(-1)).toContain('new_user_2');
+    });
+    test('Array includes five elements', () => {
+        expect(userList.lenght = 5).toBe(5);
+    });
+    test('Third element is a "string"', () => {
+        expect(typeof userList[2]).toBe('string');
+    });
+    test('Array does not consist of 8 elements', () => {
+        expect(userList.lenght).not.toBe(8);
+    });
+});
